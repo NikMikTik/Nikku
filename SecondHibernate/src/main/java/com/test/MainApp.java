14e7ebc36e6e25797fd6e9b70d18d8d29ce9683d
Nikita Jain <Nikita.jain@infogain.com>
2018-05-31 15:01:50 +0530

    Commit after adding SecondHibernate File

---
+++ SecondHibernate/src/main/java/com/test/MainApp.java
@@ -0,0 +1,19 @@
+package com.test;
+import com.test.*;
+public class MainApp {
+
+	public static void main(String[] args) {
+	
+		Admin aa=new Admin();
+		aa.Add();
+		aa.Show();
+		aa.Update();
+		aa.Show();
+		aa.Search();
+		aa.Del();
+		aa.Show();
+		
+
+	}
+
+}
